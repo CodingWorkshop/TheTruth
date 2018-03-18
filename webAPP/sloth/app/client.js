a0839512dae8db7b9a92cb075bb2a6f524b04095
cyan92128505 <cyan92128505@gmail.com>
2018-03-18 18:18:07 +0800

    rename getVideoList property

--- webAPP/sloth/app/client.js
+++ webAPP/sloth/app/client.js
@@ -1,29 +1,35 @@
 var videoLearningPlaySystem = () => {
-    var webServer = 'http://172.20.10.2:5000/';
+    var webServer = 'http://192.168.0.105:5001/';
     var defaultPoster = 'http://media.w3.org/2010/05/sintel/poster.png';
     var defaultType = 'video/mp4';
 
     var player = videojs('video-learning-player');
 
     function getVideoList() {
-        return fetch(webServer + 'GetVideoList', { method: 'get' })
-            .then(function (res) { return res.json(); })
+        return fetch(webServer + 'api/Video/GetVideoList', {
+                method: 'get'
+            })
+            .then(function (res) {
+                return res.json();
+            }).catch(function (res) {
+                console.log(res);
+            });
     }
 
     function covertToPlayList(res) {
-        return res.map(function(video) {
+        return res.map(function (video) {
             return {
                 sources: [{
-                    src: webServer + 'PlayVideo?category=' + video.Category + '&date=' + video.Date,
+                    src: webServer + 'api/Video/PlayVideo?category=' + video.category + '&date=' + video.date,
                     type: defaultType
                 }],
                 poster: defaultPoster
-            }
-        });
+            };
+        });;
     }
 
     getVideoList()
-        .then(covertToPlayList(res))
+        .then(covertToPlayList)
         .then(function (result) {
             player.playlist(result);
             player.playlistUi();
