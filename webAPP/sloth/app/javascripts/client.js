27d36555a1c31a4fcbe8505ef19fbe560d988b8e
cyan92128505 <cyan92128505@gmail.com>
2018-03-19 21:45:27 +0800

    Modify Sloth Base UI

--- webAPP/sloth/app/javascripts/client.js
+++ webAPP/sloth/app/javascripts/client.js
@@ -1,11 +1,12 @@
 var videoLearningPlaySystem = () => {
-    var webServer = 'http://192.168.0.105:5001/';
+    var webServer = '/';
     var defaultPoster = 'http://media.w3.org/2010/05/sintel/poster.png';
     var defaultType = 'video/mp4';
 
     var player = videojs('video-learning-player');
 
     function getVideoList() {
+
         return fetch(webServer + 'api/Video/GetVideoList', {
                 method: 'get'
             })
@@ -13,17 +14,30 @@ var videoLearningPlaySystem = () => {
                 return res.json();
             }).catch(function (res) {
                 console.log(res);
+                var fakeList = [];
+                for (let index = 0; index < 10; index++) {
+                    fakeList[index] = {
+                        category: 'video-' + index,
+                        date: new Date()
+                    };
+                }
+                return fakeList;
             });
     }
 
     function covertToPlayList(res) {
         return res.map(function (video) {
             return {
+                name: '影片名稱',
+                description: '影片描述',
                 sources: [{
                     src: webServer + 'api/Video/PlayVideo?category=' + video.category + '&date=' + video.date,
                     type: defaultType
                 }],
-                poster: defaultPoster
+                poster: defaultPoster,
+                thumbnail: [{
+                    src: 'http://via.placeholder.com/121x68'
+                }]
             };
         });;
     }
