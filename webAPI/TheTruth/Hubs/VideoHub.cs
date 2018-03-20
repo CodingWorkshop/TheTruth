50f8a6c6c03fcfedef9251aaf917739fbba1a8e8
ziwa47 <ziwa5656@gmail.com>
2018-03-20 23:24:37 +0800

    Check in Hubs

--- webAPI/TheTruth/Hubs/VideoHub.cs
+++ webAPI/TheTruth/Hubs/VideoHub.cs
@@ -1,13 +1,57 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using TheTruth.ViewModels;
 
 namespace TheTruth.Hubs {
 
     public class VideoHub : Hub {
-        public Task SendVideo (string user, string message) {
-            string timestamp = DateTime.Now.ToShortTimeString ();
-            return Clients.All.SendAsync ("ReceiveMessage", timestamp, user, message);
+
+        private NLog.Logger _logger;
+        public VideoHub (NLog.Logger logger) {
+            _logger = logger;
+        }
+        /// <summary>
+        /// Client 端 來取Video
+        /// </summary>
+        /// <returns></returns>
+        public Task RequestVideo () {
+            string ip = GetRemoteIpAddress ();
+            var videos = Utility.GetIpVideoDic () [ip]?.Select (r => new VideoViewModel {
+                Category = r.Category,
+                    Name = r.Name,
+                    Code = r.Code,
+                    Date = r.Date,
+            }).ToList ();
+            return Clients.Caller.SendAsync ("PlayVideo", videos);
+        }
+        /// <summary>
+        /// 連線進來
+        /// </summary>
+        /// <returns></returns>
+        public override Task OnConnectedAsync () {
+            Utility.GetIpConnetionIdDic ().TryAdd (GetRemoteIpAddress (), Context.ConnectionId);
+            return Clients.Caller.SendAsync ("PointMe", "Ok");
+        }
+
+        /// <summary>
+        /// 離線
+        /// </summary>
+        /// <param name="exception"></param>
+        /// <returns></returns>
+        public override Task OnDisconnectedAsync (Exception exception) {
+            Utility.GetIpConnetionIdDic ().Remove (GetRemoteIpAddress ());
+            return Clients.Caller.SendAsync ("PointMe", "Bye");
+        }
+
+        /// <summary>
+        /// Get IP
+        /// </summary>
+        /// <returns></returns>
+        private string GetRemoteIpAddress () {
+            return Context.Connection.RemoteIpAddress.MapToIPv4 ().ToString ();
         }
     }
 }
---
+++ webAPI/TheTruth/Utility.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using DataAccess;
+using TheTruth.ViewModels;
+
+namespace TheTruth {
+    public static class Utility {
+        private static Dictionary<string, string> IpConnetionIdDic = new Dictionary<string, string> ();
+        private static Dictionary<string, List<Video>> IpVideoDic = new Dictionary<string, List<Video>> ();
+
+        public static Dictionary<string, string> GetIpConnetionIdDic () {
+            return IpConnetionIdDic;
+        }
+        public static void SetIpConnetionIdDic (Dictionary<string, string> ipConnetionIdDic) {
+            IpConnetionIdDic = ipConnetionIdDic;
+        }
+
+        public static Dictionary<string, List<Video>> GetIpVideoDic () {
+            return IpVideoDic;
+        }
+        public static void SetIpVideoDic (Dictionary<string, List<Video>> ipVideoDic) {
+            IpVideoDic = ipVideoDic;
+        }
+    }
+}
