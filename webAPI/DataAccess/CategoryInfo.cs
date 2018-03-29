dc0b42e350a9696539a3a9aeb75dd8ad2a13ee94
李宗翰 <liitzonghann@msn.com>
2018-03-29 21:52:34 +0800

    結構翻新

---
+++ webAPI/DataAccess/CategoryInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace DataAccess
+{
+    public class CategoryInfo
+    {
+        public string DisplayName { get; set; }
+        public int Id { get; set; }
+        public string Folder { get; set; }
+    }
+}
---
+++ webAPI/TheTruth/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace TheTruth.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+        public string DisplayName { get; set; }
+    }
+}
