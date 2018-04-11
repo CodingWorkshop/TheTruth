5f917705813e83447ab5bcc350490493b5430068
l7960261 <l7960261@gmail.com>
2018-04-11 22:34:18 +0800

    API - add CustomHost for windows.service

--- webAPI/TruthAPI/Program.cs
+++ webAPI/TruthAPI/Program.cs
@@ -1,25 +1,53 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
+using TruthAPI.CustomHost;
 
 namespace TruthAPI
 {
     public class Program
     {
+        private static string pathToContentRoot;
+
         public static void Main(string[] args)
         {
-            BuildWebHost(args).Run();
+            bool isService = true;
+            if (Debugger.IsAttached || args.Contains("--console"))
+            {
+                isService = false;
+            }
+
+            pathToContentRoot = Directory.GetCurrentDirectory();
+            if (isService)
+            {
+                var pathToExe = Process.GetCurrentProcess().MainModule.FileName;
+                pathToContentRoot = Path.GetDirectoryName(pathToExe);
+            }
+
+            if (isService)
+            {
+                BuildWebHost(args).RunAsCustomService();
+            }
+            else
+            {
+                BuildWebHost(args).Run();
+            }
         }
 
         public static IWebHost BuildWebHost(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseHttpSys(options =>
+                {
+                    options.Authentication.AllowAnonymous = true;
+                    options.Authentication.Schemes = Microsoft.AspNetCore.Server.HttpSys.AuthenticationSchemes.NTLM;
+                    options.MaxConnections = 100;
+                    options.MaxRequestBodySize = 30000000;
+                    options.UrlPrefixes.Add("http://localhost:7000");
+                })
                 .UseStartup<Startup>()
+                .UseContentRoot(pathToContentRoot)
                 .Build();
     }
 }
