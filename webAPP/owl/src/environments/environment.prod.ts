9ff0baf12e5f4feca995737416925ffc22974101
l7960261 <l7960261@gmail.com>
2018-04-11 23:35:41 +0800

    owl - modify environment.prod.ts

--- webAPP/owl/src/environments/environment.prod.ts
+++ webAPP/owl/src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  hubUrl: 'http://25.20.91.239:5000/managementhub',
-  apiUrl: 'http://25.20.91.239:5000/api'
+  hubUrl: 'http://localhost:5000/managementhub',
+  apiUrl: 'http://localhost:5000/api'
 };
