8b2b21213c201a79b168e1340f1d18394381865f
李宗翰 <liitzonghann@msn.com>
2018-04-16 23:03:00 +0800

    FileGenericRepository

--- webAPI/Repository/Interface/IRepository.cs
+++ webAPI/Repository/Interface/IRepository.cs
@@ -6,12 +6,16 @@ namespace Repository.Interface
 {
     public interface IRepository<TEntity>
     {
-        IEnumerable<TEntity> Get();
+        IEnumerable<TEntity> GetAll();
+
+        IEnumerable<TEntity> Get(Func<TEntity, bool> predicate);
 
         void Add(TEntity entity);
 
-        void Update(TEntity entity);
+        void Update(TEntity entity, Func<TEntity, TEntity> updateFunc);
 
         void Delete(TEntity entity);
+        
+        void SaveChanges();
     }
 }
