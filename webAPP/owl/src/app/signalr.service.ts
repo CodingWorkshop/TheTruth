6f03da48c10d6fa6a3cfbbed74ede12fadbf6e9e
l7960261 <l7960261@gmail.com>
2018-04-24 22:39:36 +0800

    owl - signalr 完善背景重新連線機制

--- webAPP/owl/src/app/signalr.service.ts
+++ webAPP/owl/src/app/signalr.service.ts
@@ -5,11 +5,34 @@ import { environment } from '../environments/environment';
 export class SignalrService {
 
   connection: any;
+  isConnected: boolean;
 
   constructor(
   ) {
     this.connection = new signalR.HubConnection(`${environment.hubUrl}`);
-    this.connection.start();
+    this.connection.onclose(() => {
+      this.isConnected = false;
+      console.warn('實時狀態斷開, 5 秒後重新連接');
+      this.init();
+    });
+
+    this.init();
+  }
+
+  init(): void {
+    this.connection
+      .start()
+      .then(() => {
+        this.isConnected = true;
+        console.log('實時狀態正常運作');
+      })
+      .catch(err => {
+        this.isConnected = false;
+        console.error('實時狀態異常, 5 秒後重新連接');
+        setTimeout(() => {
+          this.init();
+        }, 5000);
+      });
   }
 
   on<T>(methodName: string, method: (arg: T) => void) {
